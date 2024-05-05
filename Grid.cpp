2c8d564850023d25ecf7c46f84f5ae532cd0ef5e
anannicole <21163@portalcedi.edu.mx>
2024-05-05 14:10:58 -0600

    tarea

--- Grid.cpp
+++ Grid.cpp
@@ -1,5 +1,6 @@
 #include "Grid.hpp"
 
+
 Grid::Grid(int rows, int cols)
 {
     this->rows=rows; //aqui solo iniializas las variables rols y cols que ya creaste en el .hpp
@@ -27,6 +28,13 @@ for (int i =0; 1<rows; i++) //esto es para llenar la cuaf¿dricula con 0
     
     for(int j=0; j < cols; j++) // a mi vector de vectores que crea el grid le estoy pasando un vector vacio para crear las filas 
     {
+       RectangleShape cuadrado(Vector2f(50.f, 50.f));
+       cuadrado.setOutlineThickness(5.f);
+       cuadrado.setOutlineColor(Color::White);
+       cuadrado.setPosition(200.0f, 200.0f);
+       
+       
+       
         //crear un cuadrado (RectangleShape)
        // Le damos un bode con outline thickness y color 
        // fijamos su posicion al lugar correspndiente 
